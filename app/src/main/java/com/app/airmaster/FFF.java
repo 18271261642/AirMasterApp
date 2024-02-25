def11c21d31a57b5dba8622416d5b8da082a357a
Admin <18271261642@163.com>
2024-02-26 00:55:46 +0800

    修改固件升级touchpad的接口

---
+++ app/src/main/java/com/app/airmaster/FFF.java
@@ -0,0 +1,15 @@
+package com.app.airmaster;
+
+import com.blala.blalable.Utils;
+
+public class FFF {
+
+    public static void main(String[] args) {
+        String str = "30303030303030303030000000000000";
+        byte[] array = Utils.hexStringToByte(str);
+
+        System.out.println("-------array="+new String(array));
+
+    }
+
+}
